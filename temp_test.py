3fe443c879e057d730f41a50392cf30c27a267bf
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-11-30 23:37:38 +0300

    selecion

--- temp_test.py
+++ temp_test.py
@@ -5,10 +5,22 @@ import openpyxl
 book = openpyxl.open("/Users/vladislavlipkin/Desktop/data/кросс_зд_602,пом_104.xlsx", read_only=True)
 sheet = book.active
 
-# phone = input("Введите номер телефона:")
-# for row in range(1, 708):
-#     if sheet[row][1].value == phone:
-#         print(f"Строка: {row}")
+phone = input("Введите номер телефона:")
+convert = ''
+flag = True
+for num in phone:
+    if flag:
+        convert += num
+        convert += "-"
+        flag = False
+    else:
+        convert += num
+        flag = True
 
-for row in sheet:
-    print(row[1].value)
+convert = convert.rstrip("-")
+for row in range(1, 708):
+    if sheet[row][1].value == convert:
+        print(f"Строка: {row}")
+
+# for row in sheet:
+#     print(row[1].value)
