2bd3eacf932dea1bb8eb85af48fc2c0a80a8abf9
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-12-15 22:34:58 +0300

    read_xslx

--- temp_test.py
+++ temp_test.py
@@ -1,19 +1,13 @@
 import openpyxl
 import re
 import os
+import tkinter as tk
 
-# Work main programm
-def main():
-    #
-    enemy = Search_number()
-    enemy.read_path_file()
-
-    print(enemy.output())
 
 class Search_number:
 
-    def __init__(self):
-        self.search_number = re.sub("[^0-9]", "", input("Введите номер: "))
+    def __init__(self, search_number):
+        self.search_number = search_number#re.sub("[^0-9]", "", input("Введите номер: "))
         self.data = {}
         print(f"По запросу {self.search_number}")
 
@@ -22,37 +16,83 @@ class Search_number:
 
         for file in os.listdir(directory):
             if file.endswith(".xlsx"):
-                print(file)
                 book = openpyxl.open(f"/Users/vladislavlipkin/Desktop/data/{file}", read_only=True)
                 sheet = book.active
 
                 for row in sheet:
                     if row[0].value == "$":
-                        # print(f"-{row[0].value}-")
-                        equipment_name = str(row[1].value)
-                    number = re.sub("[^0-9]", "", str(row[1].value))
-                    if number == self.search_number:
-                        self.data[file] = str(
-                            f"оборудование {equipment_name}, номер телефона {str(row[1].value)}, "
+                        key_name = row[1].value
+                    left_number = re.sub("[^0-9]", "", str(row[1].value))
+                    right_number = re.sub("[^0-9]", "", str(row[6].value))
+
+                    if left_number == self.search_number:
+                        self.data[key_name] = str(
+                            f"номер телефона {str(row[1].value)}, "
                             f"номер пары {str(row[0].value)}, "
                             f"адрес кросс.параллели {str(row[2].value)}, "
                             f"номер помещения {str(row[3].value)}")
+                        # print(file)
+                        # print("LEFT")
 
+                    if right_number == self.search_number:
+                        self.data[key_name] = str(
+                            f"номер телефона {str(row[6].value)}, "
+                            f"номер пары {str(row[5].value)}, "
+                            f"адрес кросс.параллели {str(row[7].value)}, "
+                            f"номер помещения {str(row[8].value)}")
+                        # print(file)
+                        # print("RIGHT")
     def output(self):
         return self.data
 
 
 
 # 6-27-74
+# 5-17-26
+# 5-41-81
+# 5-14-86
+# 5-55-83
 
 class Graphic_interface:
-    pass
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Text Input Output App")
+
+        self.input_label = tk.Label(self.root, text="Enter Text:")
+        self.input_label.pack()
+
+        self.input_entry = tk.Entry(self.root)
+        self.input_entry.pack()
+
+        self.submit_button = tk.Button(self.root, text="Submit", command=self.show_output)
+        self.submit_button.pack()
+
+    def show_output(self):
+        input_text = self.input_entry.get()
+        search_number = re.sub("[^0-9]", "", input_text)
+        enemy = Search_number(search_number)
+        enemy.read_path_file()
+        result = enemy.output()
+        for k, v in result.items():
+            output_label = tk.Label(self.root, text=f"название оборудования {k} {v}")
+            output_label.pack()
 
 
+
+
+def main():
+
+    root = tk.Tk()
+    app = Graphic_interface(root)
+    root.mainloop()
+
+    # while 1 != 0:
+    #     graphic = Graphic_interface()
+    #     enemy = Search_number(graphic.search_number)
+    #     enemy.read_path_file()
+    #     enemy.output()
 
 main()
-
-
 
 
 
